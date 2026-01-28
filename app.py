43f232a8b5cc2828c1db3093d73de5c2af507ca3
Sudhir <195740964+Sud123231@users.noreply.github.com>
2026-01-28 15:44:38 +0530

    content scraping from a static website

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
 
-#overview of scrapping
-with open('home.html','r') as content:
-    a=content.read()
-    soup=BeautifulSoup(a,'lxml')
-    output=soup.find_all('strong')# this returns list of tags or whatever we search for
-    #for item in output:
-    print(output.text) #or print(item.h5.text) if we want only text of h5 tags inside item       
+# #overview of scrapping
+# with open('home.html','r') as content:
+#     a=content.read()
+#     soup=BeautifulSoup(a,'lxml')
+#     output=soup.find_all('strong')[1]# this returns list of tags or whatever we search for
+#     #for item in output:
+#     print(output.text) #or print(item.h5.text) if we want only text of h5 tags inside item       
 
-
+html_text=requests.get("https://quotes.toscrape.com/").text
+soup=BeautifulSoup(html_text,'lxml')
+content=soup.find_all('div',class_="quote")
+with open('quotes.txt','w',encoding="utf-8") as f:
+    f.write('Index, Author, Quote\n')
+    for (idx,item) in enumerate(content):
+        author=item.find('small', class_='author').text
+        quote=item.span.text
+        f.write(f'{idx}, {author}, {quote}\n')
