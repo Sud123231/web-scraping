a4e47b60d2a9b8c3195665cf1d9cc2f1896ef746
Sudhir <195740964+Sud123231@users.noreply.github.com>
2026-01-28 16:14:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 # with open('home.html','r') as content:
 #     a=content.read()
 #     soup=BeautifulSoup(a,'lxml')
-#     output=soup.find_all('strong')[1]# this returns list of tags or whatever we search for
+#     output=soup.find_all('strong')# this returns list of tags or whatever we search for
 #     #for item in output:
 #     print(output.text) #or print(item.h5.text) if we want only text of h5 tags inside item       
 
